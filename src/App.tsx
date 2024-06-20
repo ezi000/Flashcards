688a2474dd95c15342658748d42441e3f94565be
ezi000 <ezio18@o2.pl>
2024-06-20 19:43:11 +0200

    Background

--- src/App.tsx
+++ src/App.tsx
@@ -46,11 +46,34 @@ function App() {
         <SignupModal open={openSignup} handleClose={handleClose} />
         <UserContext.Provider value={userId}>
           <Outlet />
+          <Background src="/logo.png" alt="logo"></Background>
+          <StyledH1>Learn faster, remember better - Join us today!</StyledH1>
         </UserContext.Provider>
       </Body>
     </>
   );
 }
+
+const StyledH1 = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #3a3838;
+  font-size: 3rem;
+  text-align: center;
+  font-weight: 900;
+  user-select: none;
+  margin: 0;
+  @media (max-width: 768px) {
+    font-size: 1.7rem;
+  }
+`;
+
+const Background = styled.img`
+  margin-top: 5rem;
+  max-width: 100%;
+  @media (max-width: 768px) {
+    max-width: 60%;
+  }
+`;
 
 const Body = styled.div`
   margin: 0;
